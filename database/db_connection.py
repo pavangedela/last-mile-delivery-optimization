6859da0d11cc7c3feaefe25c37b88412a8927a3c
pavangedela <2300033797@kluniversity.in>
2026-06-11 12:10:44 +0530

    Update database connection

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,9 +1,8 @@
 import psycopg2
 
 def get_connection():
-    conn = psycopg2.connect(
+    return psycopg2.connect(
         host="localhost",
         database="logistics_db",
         user="gedelapavan"
     )
-    return conn

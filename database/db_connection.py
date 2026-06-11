d306b46f726c815da696f5827b6be0e79df6ac95
pavangedela <2300033797@kluniversity.in>
2026-06-11 17:12:08 +0530

    Connect app to Neon database

--- database/db_connection.py
+++ database/db_connection.py
@@ -2,7 +2,9 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
-        database="logistics_db",
-        user="gedelapavan"
+        host="ep-polished-bird-aosckuge.c-2.ap-southeast-1.aws.neon.tech",
+        database="neondb",
+        user="neondb_owner",
+        password="npg_cDagZwzpMA75",
+        sslmode="require"
     )
